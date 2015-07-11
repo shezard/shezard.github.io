de586dd5eed9a5c6dd423ec4295bb6aa2eb0e82a
damien fayol <dam.fayol@gmail.com>
2015-07-11 18:15:59 +0200

    Ajout de la minification de l'index des projets

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var minifyHTML = require('gulp-minify-html');
 var inlineStyle = require('gulp-inline-style');
 var minifyInline = require('gulp-minify-inline');
 
-gulp.task('default', function() {
+gulp.task('default', ['home', 'projects'])
+
+gulp.task('home', function() {
     return gulp.src('./src/index.html')
         .pipe(minifyHTML({
           empty: true
@@ -11,4 +13,14 @@ gulp.task('default', function() {
         .pipe(inlineStyle('./'))
         .pipe(minifyInline())
         .pipe(gulp.dest('./'));
+});
+
+gulp.task('projects', function() {
+    return gulp.src('./src/projects/index.html')
+        .pipe(minifyHTML({
+          empty: true
+        }))
+        .pipe(inlineStyle('./src/css'))
+        .pipe(minifyInline())
+        .pipe(gulp.dest('./projects/'));
 });
